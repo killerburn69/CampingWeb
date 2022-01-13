4ef1aa55241a2ac460f47032a785bf2c31f0ae8c
edith-coop <baovan301@gmail.com>
2022-01-13 10:33:06 +0700

    upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const openToggle = document.querySelector('.header-icon')
+const menu = document.querySelector('.header-sign')
+const closeToggle = document.querySelector('.menu-close')
+const navbar = document.querySelector('.header-main')
+openToggle.addEventListener('click', function(e){
+    e.preventDefault()
+    menu.classList.add('isActive')
+})
+closeToggle.addEventListener('click', function(e){
+    e.preventDefault()
+    menu.classList.remove('isActive')
+})
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 1)
+    {
+        navbar.classList.add('activeNavbar')
+    }
+    else{
+        navbar.classList.remove('activeNavbar')
+    }
+})
+
+const icons = document.getElementsByClassName('explore-icon')
+console.log(icons)
+for(let i = 0;i<icons.length;i++){
+    icons[i].addEventListener('click', function(){
+        if(icons[i].style.color == 'red')
+        {
+            icons[i].style.color = 'grey'
+        }
+        else{
+            icons[i].style.color = 'red'
+        }
+    })
+}
